920e1817fa99d2a11a5912f5df9271ddc2026560
Starbugstone <pint2stone@gmail.com>
2018-03-04 23:17:02 +0100

    clean up
    No real importance

--- category.php
+++ category.php
@@ -42,13 +42,8 @@ get_header(); ?>
 						?>
 						<div class="col-md-4">
 							<?php
-							/*
-							 * Include the Post-Format-specific template for the content.
-							 * If you want to override this in a child theme, then include a file
-							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-							 */
 
-							get_template_part( 'template-parts/content_col', get_post_format() );
+							get_template_part( 'template-parts/content_col');
 							?>
 						</div>
 						<?php
