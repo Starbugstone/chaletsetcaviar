10231d1799d61ebc757cc9f9ad217447bbb5aa5b
Starbugstone <pint2stone@gmail.com>
2018-03-08 00:14:53 +0100

    setting up page.php and putting the footer realy on the bottom of the page
    Changing things up a bit

--- page.php
+++ page.php
@@ -16,23 +16,73 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<div class="container-fluid singlePost">
+				<div class="row">
 
-			<?php
-			while ( have_posts() ) : the_post();
+					<?php
+					while ( have_posts() ) : the_post();
+					?>
 
-				get_template_part( 'template-parts/content', 'page' );
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<header class="entry-header">
+								<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+							</header><!-- .entry-header -->
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				// if ( comments_open() || get_comments_number() ) :
-				// 	comments_template();
-				// endif;
+							<?php chaletsetcaviar_post_thumbnail(); ?>
 
-			endwhile; // End of the loop.
-			?>
+							<div class="entry-content">
+								<?php
+									the_content();
+
+									wp_link_pages( array(
+										'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'chaletsetcaviar' ),
+										'after'  => '</div>',
+									) );
+								?>
+							</div><!-- .entry-content -->
+
+							<?php if ( get_edit_post_link() ) : ?>
+								<footer class="entry-footer">
+									<?php
+										edit_post_link(
+											sprintf(
+												wp_kses(
+													/* translators: %s: Name of current post. Only visible to screen readers */
+													__( 'Edit <span class="screen-reader-text">%s</span>', 'chaletsetcaviar' ),
+													array(
+														'span' => array(
+															'class' => array(),
+														),
+													)
+												),
+												get_the_title()
+											),
+											'<span class="edit-link">',
+											'</span>'
+										);
+									?>
+								</footer><!-- .entry-footer -->
+							<?php endif; ?>
+						</article><!-- #post-<?php the_ID(); ?> -->
+					<?php
+						//get_template_part( 'template-parts/content', 'page' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						// if ( comments_open() || get_comments_number() ) :
+						// 	comments_template();
+						// endif;
+
+					endwhile; // End of the loop.
+					?>
+
+				</div><!-- End Row -->
+			</div> <!-- End Container-fluid -->
+
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
--- single.php
+++ single.php
@@ -78,8 +78,31 @@ get_header(); ?>
 
 					endwhile; // End of the loop.
 					?>
-				</div>
-			</div>
+				</div><!-- End Row -->
+				<?php // Edit Link 
+				if ( get_edit_post_link() ) : ?>
+					<div class="entry-footer">
+						<?php
+							edit_post_link(
+								sprintf(
+									wp_kses(
+										/* translators: %s: Name of current post. Only visible to screen readers */
+										__( 'Edit <span class="screen-reader-text">%s</span>', 'chaletsetcaviar' ),
+										array(
+											'span' => array(
+												'class' => array(),
+											),
+										)
+									),
+									get_the_title()
+								),
+								'<span class="edit-link">',
+								'</span>'
+							);
+						?>
+					</div><!-- .entry-footer -->
+				<?php endif; ?>
+			</div> <!-- End Container-fluid -->
 
 
 		</main><!-- #main -->
