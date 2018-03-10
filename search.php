1a2693af12b3899ebcaf8650814db2f3959cf338
Starbugstone <pint2stone@gmail.com>
2018-03-10 22:32:12 +0100

    no results
    No results in the search form. Display a no results phrase

--- search.php
+++ search.php
@@ -15,44 +15,49 @@ get_header(); ?>
 		<main id="main" class="site-main">
 
 		<?php
-		if ( have_posts() ) :
-			// get the current taxonomy term
-			$term = get_queried_object();
-
 			//set default background image
 			$backgroundImage = get_template_directory_uri().'/img/search.jpg';
-
 			?>
 
 			<header class="page-header">
 				<div class="full_bg_image" style="background-image: url('<?=$backgroundImage?>');"></div>
-				<h1>Recherche : <?php echo get_search_query(); ?></h1>
+				<div class="row mt-2 align-items-center">
+					<div class="col-md-8">
+						<h1>Recherche : <?php echo get_search_query(); ?></h1>
+					</div>
+					<div class="col-md-4">
+						<?php get_search_form(); ?>
+					</div>
+				</div>
 			</header><!-- .page-header -->
 			<div class="container-fluid">
 				<div class="row">
 					<?php
-					/* Start the Loop */
-					while ( have_posts() ) : the_post();
-						?>
-						<div class="col-12 my-2 px-md-5">
-							<?php
-
-							get_template_part( 'template-parts/content_line');
+					if ( have_posts() ) :
+						// get the current taxonomy term
+						$term = get_queried_object();
+						/* Start the Loop */
+						while ( have_posts() ) : the_post();
 							?>
-						</div>
+							<div class="col-12 my-2 px-md-5">
+								<?php
+
+								get_template_part( 'template-parts/content_line');
+								?>
+							</div>
+							<?php
+						endwhile;
+					else :
+						?>
+						<p>Desolé, votre recherche n'a rien retourné. Essayez un autre terme</p>
 						<?php
-					endwhile;
+					endif;
 					?>
+					</div>
 				</div>
-			</div>
-			<?php
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+				<?php
+				the_posts_navigation();
+ 				?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
