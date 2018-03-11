49144ef69289476caaa2c68bc52c6ca613ab50de
Starbugstone <pint2stone@gmail.com>
2018-03-11 10:16:17 +0100

    page 404
    Adding the 404 page

--- 404.php
+++ 404.php
@@ -1,60 +1,46 @@
 <?php
 /**
- * The template for displaying 404 pages (not found)
+ * The template for displaying archive pages
  *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package Chalets_et_caviar
  */
+
+
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'chaletsetcaviar' ); ?></h1>
-				</header><!-- .page-header -->
+		<?php
+			//set default background image
+			$backgroundImage = get_template_directory_uri().'/img/skiFall.jpg';
+			?>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'chaletsetcaviar' ); ?></p>
+			<header class="page-header">
+				<div class="full_bg_image" style="background-image: url('<?=$backgroundImage?>');"></div>
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'chaletsetcaviar' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'chaletsetcaviar' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+			</header><!-- .page-header -->
+			<div class="container-fluid">
+				<div class="row mt-2 align-items-center justify-content-md-center">
+					<div class="col-md-5 col-lg-4">
+						<h1>Il n'y a rien ici</h1>
+						<p>Essayez d'utilizer la recherche</p>
+					</div>
+					<div class="col-md-4">
+						<?php get_search_form(); ?>
+					</div>
+				</div>
+			</div>
+				<?php
+				the_posts_navigation();
+ 				?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
+//get_sidebar();
 get_footer();
