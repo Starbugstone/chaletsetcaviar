1993404b639518ff44b9456ae53ecc3d3a7f0253
StarbugStone <pint2stone@gmail.com>
2018-03-21 22:49:11 +0100

    accessabilty correction
    change h1 to h2

--- page.php
+++ page.php
@@ -25,7 +25,7 @@ get_header(); ?>
 
 					<article id="post-<?php the_ID(); ?>" <?php post_class('col-md-10 category-listing'); ?>>
 						<header class="entry-header">
-							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+							<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 						</header><!-- .entry-header -->
 
 						<?php chaletsetcaviar_post_thumbnail(); ?>
